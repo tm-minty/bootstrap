0819c32bb31ad5be3f63b7d0cac0d894fd4017a7
Timur Mingaliev <timur@mingaliev.com>
2015-09-25 00:23:56 +0300

    Remove self after initialization

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,8 +17,7 @@ cp ./.bootstrap/gulp/watch.js       gulpdir
 cp ./.bootstrap/gulp/server.js      gulpdir
 
 echo "Configuring project"
-echo "Project directory [html]:"
-read PROJECT_DIR
+read -p "Project directory [html]: " PROJECT_DIR
 if [ -z "$PROJECT_DIR" ]; then
     PROJECT_DIR="html"
 fi
@@ -26,24 +25,21 @@ mkdir -p $PROJECT_DIR
 sed -i "s/\\\$PROJECT_DIR\\\$/$(echo ${PROJECT_DIR} | sed -e 's/[\/&]/\\&/g')/g" "gulpdir/paths.js";
 cp ./.bootstrap/index.html $PROJECT_DIR
 
-echo "Build directory [.tmp]:"
-read BUILD_DIR
+read -p "Build directory [.tmp]: " BUILD_DIR
 if [ -z "$BUILD_DIR" ]; then
     BUILD_DIR=".tmp"
 fi
 mkdir -p $PROJECT_DIR/$BUILD_DIR
 sed -i "s/\\\$BUILD_DIR\\\$/$(echo ${BUILD_DIR} | sed -e 's/[\/&]/\\&/g')/g" "gulpdir/paths.js";
 
-echo "Styles directory [styles]:"
-read STYLES_DIR
+read -p "Styles directory [styles]: " STYLES_DIR
 if [ -z "STYLES_DIR" ]; then
     STYLES_DIR="styles"
 fi
 mkdir -p $PROJECT_DIR/$STYLES_DIR
 sed -i "s/\\\$STYLES_DIR\\\$/$(echo ${STYLES_DIR} | sed -e 's/[\/&]/\\&/g')/g" "gulpdir/paths.js";
 
-echo "Scripts directory [scripts]:"
-read SCRIPTS_DIR
+read -p "Scripts directory [scripts]: " SCRIPTS_DIR
 if [ -z "SCRIPTS_DIR" ]; then
     SCRIPTS_DIR="scripts"
 fi
@@ -52,11 +48,17 @@ sed -i "s/\\\$SCRIPTS_DIR\\\$/$(echo ${SCRIPTS_DIR} | sed -e 's/[\/&]/\\&/g')/g"
 
 echo "Configuring Bower"
 bower init
-echo "Bower directory [${PROJECT_DIR}/bower_components]: "
-read BOWER_DIRECTORY
+read -p "Bower directory [${PROJECT_DIR}/bower_components]: " BOWER_DIRECTORY
 if [ -z "BOWER_DIRECTORY" ]; then
     BOWER_DIRECTORY="${PROJECT_DIR}/bower_components"
 fi
 cp ./.bootstrap/.bowerrc .
 sed -i "s/\\\$BOWER_DIRECTORY\\\$/$(echo ${BOWER_DIRECTORY} | sed -e 's/[\/&]/\\&/g')/g" ".bowerrc";
 sed -i "s/\\\$BOWER_DIRECTORY\\\$/$(echo ${BOWER_DIRECTORY} | sed -e 's/[\/&]/\\&/g')/g" "gulpdir/inject.js";
+
+read -n1 -p "Remove self? [y/N]: " REMOVE_SELF
+if [[ $REMOVE_SELF = "y" ]]; then
+    rm -rf .git
+    rm -rf .bootstrap
+    rm -rf bootstrap.sh
+fi
